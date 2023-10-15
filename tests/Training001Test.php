9d87d01a28b7f530302b936a6b104815bd247baf
こぴぺたん <takahiro.inoue.0508+github@gamail.com>
2023-10-15 16:51:13 +0000

    スキップを導入

--- tests/Training001Test.php
+++ tests/Training001Test.php
@@ -9,13 +9,19 @@ class Training001Test extends TestCase
      */
     public function testHelloWorld()
     {
-        // 環境変数からPORTを取得、未設定なら80をデフォルトとする
+        $filePath = __DIR__ . '../training/001/index.php';
+        // 解答ファイルが存在するか確認
+        if (!file_exists($filePath)) {
+            $this->markTestSkipped('File training/001/index.php does not exist.');
+            return;
+        }
+
         $port = getenv('PORT') ?: '80';
+        $url = "http://localhost:{$port}/001";
+        // file_get_contentsを使用してHTTPリクエストを実行
+        $response = file_get_contents($url);
 
-        // file_get_contentsでHTTP GETリクエストを送信
-        $response = file_get_contents("http://localhost:{$port}/001");
-
-        // コンテンツの確認
-        $this->assertEquals('Hello, World!', $response);
+        // アサーション（テスト結果の検証）
+        $this->assertEquals("Hello, World!", trim($response));
     }
 }
